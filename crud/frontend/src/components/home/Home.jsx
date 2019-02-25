3a840e5ccae00d6d450f9b9dc9b49109b3f4a71e
Juliancio Alves de Carvalho <juliancio_alves@hotmail.com>
2019-02-24 21:26:24 -0300

    first commit

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+<Main icon="home" title="Início"
+    subtitle="Projeto Cadastro de Usuários">
+        <div className='display-4'>Bem vindo!</div>
+        <hr />
+    <p className="mb-0">Um sistema simples para a realização de cadastro de usuários.</p>
+</Main>
